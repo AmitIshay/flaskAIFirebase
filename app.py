87ffb5f9d0f8be9df215812473febd384b259742
AmitIshay <109962844+AmitIshay@users.noreply.github.com>
2025-01-24 17:29:15 +0200

    Updated

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from flask_cors import CORS
 import openai
-import os
+import os, json
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
-# Initialize Firebase
-firebase_key_path = os.getenv("FIREBASE_KEY_PATH")
-cred = credentials.Certificate(firebase_key_path)
-firebase_admin.initialize_app(cred)
+firebase_key = os.getenv("FIREBASE_KEY")
+if firebase_key:
+    try:
+        cred = credentials.Certificate(json.loads(firebase_key))
+        firebase_admin.initialize_app(cred)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Error parsing Firebase key: {str(e)}")
+else:
+    raise ValueError("Firebase key is missing in environment variables.")
+
 
 
 # חיבור למסד נתונים Firestore
